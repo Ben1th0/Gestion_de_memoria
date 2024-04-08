a7beb4a967ea1c93568dcc1214f0f19bf8720bd6
Tiago Benitez <osbenith02@gmail.com>
2024-04-07 19:58:50 -0500

    inicio de segmentacion

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -3,6 +3,9 @@ const pila = 65536;
 //monticulo
 const monticulo = 131072;
 
+//offset
+let limite;
+
 // Función para generar programas automáticamente
 function generarProgramasAutomaticos() {
     // Crear 5 programas automáticamente
@@ -433,14 +436,29 @@ function terminarProceso(index) {
 function abrirMiniVentana() {
     var estatica = document.getElementById("Estatica");
     var estaticaVar = document.getElementById("EstaticaVar");
+    var segmentacion = document.getElementById("Segmentacion");
+    var paginacion = document.getElementById("Paginacion");
+    
     if (estatica.checked){
+        document.getElementById("texto-particiones").innerText = "Coloca las dimenciones de la memoria"
         document.querySelector(".particiones").style.display = 'none';
         document.getElementById("miniVentana").style.display = 'block';
         document.getElementById('btn-crearPart').style.display = 'none';
     }else if(estaticaVar.checked){
+        document.getElementById("texto-particiones").innerText = "Coloca las dimenciones de la memoria"
         document.getElementById('miniVentana').style.display = 'block';
         document.querySelector(".particiones").style.display = 'block';
         document.getElementById('btn-crearPart').style.display = 'inline';
+    }else if(segmentacion.checked){
+        document.getElementById("texto-particiones").innerText = "colocar segmentacion en bytes"
+        document.querySelector(".particiones").style.display = 'none';
+        document.getElementById("miniVentana").style.display = 'block';
+        document.getElementById('btn-crearPart').style.display = 'none';
+    }else if(paginacion.checked){
+        document.getElementById("texto-particiones").innerText = "colocar paginacion en bytes"
+        document.querySelector(".particiones").style.display = 'none';
+        document.getElementById("miniVentana").style.display = 'block';
+        document.getElementById('btn-crearPart').style.display = 'none';
     }else{
         crearsimulador();
     }
@@ -633,10 +651,13 @@ function crearsimulador(){
     var estatica = document.getElementById("Estatica");
     var dinamica = document.getElementById("Dinamica");
     var dinamicaCom = document.getElementById("DinamicaCom");
+    var segmentacion = document.getElementById("Segmentacion");
+    var paginacion = document.getElementById("Paginacion");
+
     if (estatica.checked){
         programas = [];
         listaParticiones = []; 
-        var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'}
+        var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'};
         listaParticiones.push(SO);
         var memoria = 15728640 ;
         var parttam = parseInt(document.getElementById("tamPart").value);
@@ -650,20 +671,118 @@ function crearsimulador(){
             }
             console.log(i);
         }
+        interfaz1();
     }else if(dinamica.checked){
         tipoMemorita = 1;
         listaParticiones = [];
         procesos = []
         var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'}
         listaParticiones.push(SO);
+        interfaz1();
     }else if(dinamicaCom.checked){
         tipoMemorita = 2;
         listaParticiones = [];
         procesos = []
         var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'}
         listaParticiones.push(SO);
+        interfaz1();
+    }else if(segmentacion.checked){
+        programas = [];
+        listaParticiones = []; 
+        var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'};
+        var libre = {estado: 'libre', tamaño: 15728640, proceso: '', nombre: ''};
+        listaParticiones.push(SO);
+        listaParticiones.push(libre);
+        var memoria = 15728640 ;
+        var parttam = parseInt(document.getElementById("tamPart").value);
+        var tambytes = 1;
+        for(let i=0;i<parttam;i++){
+            tambytes *= 2;
+        }
+        limite = 16777216 / tambytes;
+        interfaz2();
+        console.log(listaParticiones);
+    }else if(paginacion.checked){
+        programas = [];
+        listaParticiones = []; 
+        var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'};
+        listaParticiones.push(SO);
+        var memoria = 15728640 ;
+        var parttam = parseInt(document.getElementById("tamPart").value);
+        var tambytes = 1;
+        for(let i=0;i<parttam;i++){
+            tambytes *= 2;
+        }
+        limite = 16777216 / tambytes;
+        for (let i = memoria ; i > 0 ; i -= limite){
+            if(i<limite){
+                var parti = {estado: 'libre', tamaño: i, proceso: '', nombre: ''}
+                listaParticiones.push(parti);
+            }else{
+                var parti = {estado: 'libre', tamaño: limite, proceso: '', nombre: ''}
+                listaParticiones.push(parti);
+            }
+            console.log(i);
+        }
+        interfaz2();
     }
 
+    document.getElementById('caja-principal').style.display = 'grid';
+    document.getElementById('caja-principal').style.gridTemplateColumns = '1fr 2fr';
+
+    crearParticionesGraficas();
+    generarProgramasAutomaticos();
+}
+
+//funcion para volver al inicio
+function volverInicio(){
+    programas = [];
+    listaParticiones = [];
+    var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'}
+    listaParticiones.push(SO);
+    document.getElementById("caja-principal").innerHTML = "";
+    document.getElementById("caja-principal").innerHTML =`
+    
+    <div class='caja-menu'>
+        <h2 class='sub-titulo'>Escoja el tipo de particion de memoria</h2>
+        <div class='opciones'>
+            <div class='sub-opciones'><input type='radio' name='particion' id='Estatica'><label for='Estatica'>Particion Estatica</label></div>
+            <div class='sub-opciones'><input type='radio' name='particion' id='EstaticaVar'><label for='EstaticaVar'>Particion Estatica Variable</label></div>
+            <div class='sub-opciones'><input type='radio' name='particion' id='Dinamica'><label for='Dinamica'>Particion Dinamica</label></div>
+            <div class='sub-opciones'><input type='radio' name='particion' id='DinamicaCom'><label for='DinamicaCom'>Particion Dinamica compacta</label></div>
+            <div class='sub-opciones'><input type='radio' name='particion' id='Segmentacion'><label for='Segmentacion'>Segmentacion</label></div>
+            <div class='sub-opciones'><input type='radio' name='particion' id='Paginacion'><label for='Paginacion'>Paginacion</label></div>
+        </div>                
+        <button class='crear' onclick="abrirMiniVentana()">crear</button>
+        <div id="miniVentana" class="miniVentana">
+            <div class="contenido">
+                <span class="cerrar" onclick="cerrarMiniVentana()">X</span>
+                <h2>Particionar memoria</h2>
+                <p id="texto-particiones">Coloca las dimenciones de la memoria</p>
+                <div class="particiones">
+                    <div class="part-creadas">
+                        <h3>particiones <br>creadas</h3>
+                        <ul class="List-part" id="List-part">
+                        </ul>
+                    </div>
+                    <div class="part-disponible">
+                        <h3>disponible</h3>
+                        <p class="part-dispo-text" id="part-dispo-text">15728640</p>
+                    </div>
+                </div>
+            <div class="crear-particiones">
+                <input type="number" name="tamPart" id="tamPart" min="0" step="1" placeholder="tamaño de particion">
+                <button for="tamPart" id="btn-crearPart" onclick="crearParticion()">Crear<br>particion</button>
+            </div>
+            <button class="crear-simu" id="crear-simu" onclick="crearsimulador()">Crear simulador</button>
+        </div>
+    </div>
+    `
+    document.getElementById('caja-principal').style.display = 'flex';
+    document.getElementById('caja-principal').style.gridTemplateColumns = '';
+     }
+
+function interfaz1(){
     document.getElementById("caja-principal").innerHTML = "";
     document.getElementById("caja-principal").innerHTML = 
     `
@@ -687,6 +806,7 @@ function crearsimulador(){
                     <th>Proceso</th>
                     <th>Tamaño</th>
                     <th>Acciones</th>
+                    <th>tabla</th>
                 </thead>
                 <!-- Cuerpo de la tabla -->
                 <tbody class="tb-proc-cu" id="tablaProcesos">
@@ -744,55 +864,95 @@ function crearsimulador(){
         </div>
     </div>
     `;
-    document.getElementById('caja-principal').style.display = 'grid';
-    document.getElementById('caja-principal').style.gridTemplateColumns = '1fr 2fr';
-
-    crearParticionesGraficas();
-    generarProgramasAutomaticos();
 }
 
-//funcion para volver al inicio
-function volverInicio(){
-    programas = [];
-    listaParticiones = [];
-    var SO = {estado: 'ocupado', tamaño: 1048576, proceso: 'SO', nombre: 'Sistema Operativo'}
-    listaParticiones.push(SO);
+function interfaz2(){
     document.getElementById("caja-principal").innerHTML = "";
-    document.getElementById("caja-principal").innerHTML =`
-    
-    <div class='caja-menu'>
-        <h2 class='sub-titulo'>Escoja el tipo de particion de memoria</h2>
-        <div class='opciones'>
-            <div class='sub-opciones'><input type='radio' name='particion' id='Estatica'><label for='Estatica'>Particion Estatica</label></div>
-            <div class='sub-opciones'><input type='radio' name='particion' id='EstaticaVar'><label for='EstaticaVar'>Particion Estatica Variable</label></div>
-            <div class='sub-opciones'><input type='radio' name='particion' id='Dinamica'><label for='Dinamica'>Particion Dinamica</label></div>
-            <div class='sub-opciones'><input type='radio' name='particion' id='DinamicaCom'><label for='DinamicaCom'>Particion Dinamica compacta</label></div>
-        </div>                
-        <button class='crear' onclick="abrirMiniVentana()">crear</button>
+    document.getElementById("caja-principal").innerHTML = 
+    `
+    <div class="memoria">
+    <h2>16 MG</h2>
+    <div class="memoria-graf">
+        <div class="repr-memoria" id="repr-memoria">
+            <div class="img-SO"><p>Sistema Operativo</p></div>
+        </div>
+        <div class="divi-memoria"></div>
+    </div>
+    </div>
+    <div class="configuraciones">
+        <div class="cont-conf procesos">
+            <h2>Procesos</h2>
+            
+            <!-- Tabla de procesos existentes -->
+            <table class="tb-procesos">
+                <!-- Encabezado de la tabla -->
+                <thead class="tb-proc-enc">
+                    <th>Proceso</th>
+                    <th>Tamaño</th>
+                    <th>Acciones</th>
+                    <th>tabla</th>
+                </thead>
+                <!-- Cuerpo de la tabla -->
+                <tbody class="tb-proc-cu" id="tablaProcesos">
+                    <!-- Filas de procesos se agregarán dinámicamente aquí -->
+                </tbody>
+            </table>
+        </div>
+        
+
+        <div class="cont-conf programa" id="contPrograma">
+        <h2>Programa</h2>
+        <!-- Agregar formulario para agregar programas -->
         <div id="miniVentana" class="miniVentana">
-        <div class="contenido">
-        <span class="cerrar" onclick="cerrarMiniVentana()">X</span>
-        <h2>Particionar memoria</h2>
-        <p>Coloca las dimenciones de la memoria</p>
-        <div class="particiones">
-            <div class="part-creadas">
-                <h3>particiones <br>creadas</h3>
-                <ul class="List-part" id="List-part">
-                </ul>
-            </div>
-            <div class="part-disponible">
-                <h3>disponible</h3>
-                <p class="part-dispo-text" id="part-dispo-text">15728640</p>
-            </div>
+            <span class="cerrar" onclick="cerrarMiniVentana()">X</span>
+            <div>
+                <form id="formPrograma">
+                    <label for="nombrePrograma">Nombre:</label>
+                    <input type="text" id="nombrePrograma" required>
+                    <label for="textPrograma">.text:</label>
+                    <input type="number" id="textPrograma" min="0" step="1" required>
+                    <label for="dataPrograma">.data:</label>
+                    <input type="number" id="dataPrograma" min="0" step="1" required>
+                    <label for="bssPrograma">.bss:</label>
+                    <input type="number" id="bssPrograma" min="0" step="1" required>
+                    <button type="button" onclick="agregarPrograma()">Agregar Programa</button>
+                </form>
+            </div> 
         </div>
-        <div class="crear-particiones">
-            <input type="number" name="tamPart" id="tamPart" min="0" step="1" placeholder="tamaño de particion">
-            <button for="tamPart" id="btn-crearPart" onclick="crearParticion()">Crear<br>particion</button>
+        <!-- Tabla de programas existentes -->
+        <table class="tb-procesos">
+            <!-- Encabezado de la tabla -->
+            <thead class="tb-proc-enc">
+                <th>Programa</th>
+                <th>tamaño codigo</th>
+                <th>tamaño datos inicializados</th>
+                <th>tamaño datos sin inicializar</th>
+                <th>Memoria a usar</th>
+                <th>" "</th>
+            </thead>
+            <!-- Cuerpo de la tabla -->
+            <tbody class="tb-proc-cu" id="tablaProgramas">     
+            </tbody>
+        </table>
+    </div>
+        <div class="cont-conf ajuste">
+            <h2>tabla de bloques libres</h2>
+            <table class="blq-libre">
+                <!-- Encabezado de la tabla -->
+                <thead class="blq-libre-enc">
+                    <th>base</th>
+                    <th>limite</th>
+                    <th>tamaño</th>
+                </thead>
+                <!-- Cuerpo de la tabla -->
+                <tbody class="blq-libre-cue">     
+                </tbody>
+            </table>
         </div>
-        <button class="crear-simu" id="crear-simu" onclick="crearsimulador()">Crear simulador</button>
+        <div class="cont-conf botones">
+            <button class="btn-boton" id="agregar-programa" onclick="VenAgrPrograma()">Agregar programa</button>
+            <button class="btn-boton" id="volver-inicio" onclick="volverInicio()">Volver al inicio</button>
         </div>
     </div>
-    `
-    document.getElementById('caja-principal').style.display = 'flex';
-    document.getElementById('caja-principal').style.gridTemplateColumns = '';
-     }
+    `;
+}
